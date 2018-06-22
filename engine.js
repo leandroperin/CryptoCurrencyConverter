329aa2360ae6edbf8ef8d7df434d95c32cd2a9be
Leandro Perin <leandro.perino@gmail.com>
2018-06-22 17:20:12 -0300

    Added i18next

--- engine.js
+++ engine.js
@@ -1,4 +1,82 @@
 $(document).ready(function() {
+  var resourcesT = {
+    "en": {
+      "translation": {
+        "language": "Language",
+        "title": "Converter",
+        "subtitle": "Select the currencies, input the amount, click Convert. Easy like this.",
+        "cryptocurr": "Crypto currency",
+        "fiatcurr": "Fiat currency",
+        "usa": "United States of America - USD",
+        "brazil": "Brazil - BRL",
+        "uk": "United Kingdom - GBP",
+        "ue": "European Union - EUR",
+        "amount": "Amount",
+        "convert": "Convert",
+        "copyright": "Made by Leandro Perin de Oliveira",
+        "ufsc": "Federal University of Santa Catarina"
+      }
+    },
+    "pt": {
+      "translation": {
+        "language": "Idioma",
+        "title": "Conversor",
+        "subtitle": "Selecione as moedas, digite a quantidade, clique Converter. Fácil assim.",
+        "cryptocurr": "Criptomoeda",
+        "fiatcurr": "Moeda fiduciária",
+        "usa": "Estados Unidos da América - DÓLAR",
+        "brazil": "Brasil - REAL",
+        "uk": "Reino Unido - LIBRA ESTERLINA",
+        "ue": "União Européia - EURO",
+        "amount": "Quantidade",
+        "convert": "Converter",
+        "copyright": "Desenvolvido por Leandro Perin de Oliveira",
+        "ufsc": "Universidade Federal de Santa Catarina"
+      }
+    },
+    "ar": {
+      "translation": {
+        "language": "لغة",
+        "title": "محول",
+        "subtitle": "حدد القطع النقدية ، اكتب الكمية ، انقر فوق تحويل. من السهل من هذا القبيل.",
+        "cryptocurr": "عملة معماة",
+        "fiatcurr": "عملة ائتمانية",
+        "usa": "الولايات المتحدة الأمريكية - الدولار",
+        "brazil": "البرازيل - ريال مدريد",
+        "uk": "المملكة المتحدة - الجنيه الاسترليني",
+        "ue": "الاتحاد الأوروبي - اليورو",
+        "amount": "كمية",
+        "convert": "تحول",
+        "copyright": "وضعت من قبل لياندرو بيرين دي أوليفيرا",
+        "ufsc": "جامعة فيدرالية سانتا كاتارينا"
+      }
+    }
+  };
+
+  i18next.init({
+    lng: 'en',
+    debug: true,
+    resources: resourcesT
+  }, function(err, t) {
+    updateContent();
+  });
+
+  function updateContent() {
+    $("#language").html(i18next.t('language'));
+    $("#title").html(i18next.t('title'));
+    $("#subtitle").html(i18next.t('subtitle'));
+    $("#cryptocurr").html(i18next.t('cryptocurr'));
+    $("#fiatcurr").html(i18next.t('fiatcurr'));
+    $("#usa").html(i18next.t('usa'));
+    $("#brazil").html(i18next.t('brazil'));
+    $("#uk").html(i18next.t('uk'));
+    $("#ue").html(i18next.t('ue'));
+    $("#amountlbl").html(i18next.t('amount'));
+    $("#convert").html(i18next.t('convert'));
+    $("#copyright").html(i18next.t('copyright'));
+    $("#ufsc").html(i18next.t('ufsc'));
+  };
+
   $(".dropdown-trigger").dropdown();
   $("select").formSelect();
 
@@ -16,20 +94,25 @@ $(document).ready(function() {
   $("#en_lang").click(function() {
     $("html").attr("lang", "en");
     $("html").attr("dir", "ltr");
+
+    i18next.changeLanguage('en');
+    updateContent();
   });
 
   $("#pt_lang").click(function() {
     $("html").attr("lang", "pt");
     $("html").attr("dir", "ltr");
 
-    $.ajax({url: "i18n/pt.json", success: function(text) {
-
-    }});
+    i18next.changeLanguage('pt');
+    updateContent();
   });
 
   $("#ar_lang").click(function() {
     $("html").attr("lang", "ar");
     $("html").attr("dir", "rtl");
+
+    i18next.changeLanguage('ar');
+    updateContent();
   });
 
   $("#convert").trigger("click");
